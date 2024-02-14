162910873d62d8f0ac2aea210a595a97f33ee6f2
Jayant Bhadauria <jayant@yopmail.com>
2024-02-14 22:17:07 +0530

    Commit1.1

---
+++ src/test/java/resourceFiles/ExtentReportsClass.java
@@ -0,0 +1,24 @@
+package resourceFiles;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentReportsClass {
+    public ExtentReports getObjectReport() {
+        String path = System.getProperty("user.dir") + "//reports//index.html";
+        
+        // Create an ExtentSparkReporter and attach it to the ExtentReports instance
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(reporter);
+
+        // Additional configurations (if needed)
+        reporter.config().setDocumentTitle("uAlgos Test Report");
+        reporter.config().setReportName("Test Automation Results");
+        
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Jayant Bhadauria");
+
+        return extent;
+    }
+}
