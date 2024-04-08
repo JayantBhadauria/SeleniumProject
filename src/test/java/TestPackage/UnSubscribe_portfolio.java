8674c10a46a971aa665ea3fddc659d9e5d1f8e30
JayantBhadauria <jayant@yopmail.com>
2024-04-08 21:03:24 +0530

    Commit 3.18

--- src/test/java/TestPackage/UnSubscribe_portfolio.java
+++ src/test/java/TestPackage/UnSubscribe_portfolio.java
@@ -1,18 +1,19 @@
 package TestPackage;
 
 import org.apache.logging.log4j.core.Logger;
-import org.testng.annotations.Test;
+
+import org.testng.annotations.*;
 import MainModules.MyPortfolio;
 import TestComponents.*;
 
 public class UnSubscribe_portfolio extends BaseTest{
-	
+	@Parameters("portfolioName")
 	@Test(groups = "Smoke",retryAnalyzer=RetryAnalyzer.class, testName="UnSubscribe portfolio")
-	public void unSubscribe() throws InterruptedException {
+	public void unSubscribe(String portfolioName) throws InterruptedException {
 		// TODO Auto-generated constructor stub
 		LoginPage.LoginApplication(prop.getProperty("username"),prop.getProperty("password"));
 		MyPortfolio myPortfolio = new MyPortfolio(driver);
 		myPortfolio.goToMyportfolioPage();
-		myPortfolio.UnSubscribePortfolio("Brahmastra");
+		myPortfolio.UnSubscribePortfolio(portfolioName);
 	}
 }
