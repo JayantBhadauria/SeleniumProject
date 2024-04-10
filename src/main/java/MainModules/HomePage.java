478f7ff5c5c7a14b6f4db12ec73f82869800ea6f
JayantBhadauria <jayant@yopmail.com>
2024-04-10 23:59:38 +0530

    Commit 3.21

--- src/main/java/MainModules/HomePage.java
+++ src/main/java/MainModules/HomePage.java
@@ -2,30 +2,28 @@ package MainModules;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import AbstractClasses.AbstractClass;
-import freemarker.log.Logger;
+
 
 public class HomePage extends AbstractClass{
-		Logger log= (Logger)LogManager.getLogger(HomePage.class);
 		WebDriver driver;
+		Logger log=(Logger) LogManager.getLogger(getClass());
 		public HomePage(WebDriver driver) {
 			super(driver);
 			this.driver=driver;
 		}
-		
-
 		public WebElement findPortfolio(String portfolioName,String section) {
 			Actions actions=new Actions(driver);
 			List<WebElement>sectionsList=driver.findElements(By.xpath("//app-marketplace-cards"));
 			WebElement portfolioCard=null;
 			for(int i=0;i<sectionsList.size();i++) {
 				String sectionName=sectionsList.get(i).findElement(By.xpath("./div/div/h2")).getText();
-				System.out.println(sectionName);
 				if(sectionName.contains(section)) {
 					WebElement inputfield=sectionsList.get(i).findElement(By.xpath(".//input"));
 					actions.click(inputfield).sendKeys(portfolioName).build().perform();
@@ -45,8 +43,38 @@ public class HomePage extends AbstractClass{
 				log.info(Response);
 			}
 			else {
-				log.error(portfolioName+" Not found");
+				log.error(portfolioName+" not found");
 			}
 		}
+
+		public void UnSubscribePortfolio(String portfolioName){
+			WebElement portfolioCard=findPortfolio(portfolioName,"Originals");
+			if(portfolioCard!=null){
+				portfolioCard.findElement(By.xpath(".//app-strategy-card/div[1]/mat-card[1]/div[1]/div[1]/div[2]/div/button[2]")).click();
+				driver.findElement(By.xpath("//div[@class='cdk-overlay-container']/div[2]/div/div/div/button[1]")).click();
+                driver.findElement(By.xpath("//app-confirmation-dialog/div/div[2]/div[2]/button")).click();
+                String Response=driver.findElement(By.xpath("//div[@id='toast-container']/app-custom-toaster/div/div/div/div")).getText();
+                log.info(Response);
+			}
+			else{
+				log.error(portfolioName + " not found");
+			}
+		}
+
+		public void CopyPortfolio(String portfolioName,String Section) throws InterruptedException{
+			WebElement portfolioCard=findPortfolio(portfolioName,Section);
+			if(portfolioCard!=null){
+				sleep(2000);
+				portfolioCard.findElement(By.xpath(".//app-strategy-card/div[1]/mat-card[1]/div[1]/div[1]/div[2]/div/button[2]")).click();
+                driver.findElement(By.xpath("//div[@class='cdk-overlay-container']/div[2]/div/div/div/button[3]")).click();
+                String Response=driver.findElement(By.xpath("//div[@id='toast-container']/app-custom-toaster/div/div/div/div")).getText();
+                log.info(Response);
+			}
+			else{
+				log.error(portfolioName + " not found");
+			}
+		}
+
+
 
 }
--- src/test/java/TestPackage/Copy_functionality.java
+++ src/test/java/TestPackage/Copy_functionality.java
@@ -3,20 +3,32 @@ package TestPackage;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import MainModules.HomePage;
 import MainModules.MyPortfolio;
 import TestComponents.BaseTest;
 import TestComponents.RetryAnalyzer;
 
 public class Copy_functionality extends BaseTest{
-	@Parameters("portfolioName")
-	@Test(groups = "Smoke",retryAnalyzer=RetryAnalyzer.class, testName="Copy portfolio")
+	// @Parameters("portfolioName1")
+	// @Test(groups = "Smoke",retryAnalyzer=RetryAnalyzer.class, testName="Copy portfolio MyPortfolio")
 	
-	public void createCopy(String portfolioName) throws InterruptedException {
+	// public void createCopy(String portfolioName) throws InterruptedException {
+	// 	// TODO Auto-generated method stub
+	// 	LoginPage.LoginApplication(prop.getProperty("username"), prop.getProperty("password"));
+	// 	MyPortfolio myPortfolio = new MyPortfolio(driver);
+	// 	myPortfolio.goToMyportfolioPage();
+	// 	myPortfolio.bookmarkedTab();
+	// 	myPortfolio.CopyPortfolio(portfolioName);
+	// }
+
+	@Parameters("portfolioName2")
+	@Test(groups = "Smoke",retryAnalyzer=RetryAnalyzer.class, testName="Copy portfolio Home")
+	public void createCopyFromHomePage(String portfolioName) throws InterruptedException {
 		// TODO Auto-generated method stub
 		LoginPage.LoginApplication(prop.getProperty("username"), prop.getProperty("password"));
-		MyPortfolio myPortfolio = new MyPortfolio(driver);
-		myPortfolio.goToMyportfolioPage();
-		myPortfolio.bookmarkedTab();
-		myPortfolio.CopyPortfolio(portfolioName);
+		HomePage home=LoginPage.getHomePage();
+		home.CopyPortfolio(portfolioName, "Created");
 	}
+
+	
 }
