54ecc73cff9d9873d7d77f4689a148992cad1a50
JayantBhadauria <jayant@yopmail.com>
2024-04-14 17:12:25 +0530

    Revert "Commit to testing git"
    This reverts commit 519ef8ef5b6d9232d8217f1aebb21e63aaabd636.

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -103,7 +103,7 @@ public class BaseTest extends DataReader{
 		@AfterMethod()
 		public void closeDriver() {
 			driver.quit();
-		    log.info("Program completed by Jayant");
+		    log.info("Program completed");
 		}
 		
 		@DataProvider()
